405995c3fe4e2c954f2b7d9ce92e1d29b7dfe334
mighTea <kylef217@gmail.com>
2023-06-21 12:13:06 -0700

    "Second Commit - Updated helloWorld for input"

--- Coding Practice/helloWorld.cpp
+++ Coding Practice/helloWorld.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string>
 
 // This code is a fucking disaster
 
 int main (){
     std::cout << "I like Hot Wings" << '\n';
     std::cout << "I like Donuts" << '\n';
+
+    std::string userFood;
+    std::cout << "Enter a food you like: ";
+    std::getline(std::cin, userFood);
+    std::cout << "I like " << userFood << " too!\n";
     return 0;
 }
-/*
-
-what the fuck am i doing
-
-*/
